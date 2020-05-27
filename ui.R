0c7dbb06d171cb4579c3d4334eaf5c153e5d8abc
ijrossi <ijrossi@uc.cl>
2020-05-26 23:00:32 -0400

    Se agrega hc_confirmados_rango_edad

--- ui.R
+++ ui.R
@@ -5,7 +5,8 @@ shinyUI(
         titlePanel("Dashboard-Covid19"),
         
         column(6, highchartOutput("hc_confirmados")),
-        column(6, highchartOutput("hc_casos_por100mh"))
+        column(6, highchartOutput("hc_casos_por100mh")),
+        column(6, highchartOutput("hc_confirmados_rango_edad"))
         
         )
     )
