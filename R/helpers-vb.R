f01b251d7cc07b5aae7bb046f3e15619de81617b
hsgonzalmu <hsgonzalmu@gmail.com>
2020-06-04 05:14:09 +0000

    cambiando la r de vb ventiladores

--- R/helpers-vb.R
+++ R/helpers-vb.R
@@ -305,7 +305,7 @@ grafico_vb_ventiladores<- function(){
   
   valueBoxSpark(
     value = lbl,
-    subtitle = HTML(paste0(round(100 * (d %>% pull(y) %>% last())), " ventiladorres disponibles<br> por cada 100")),
+    subtitle = HTML(paste0(round(100 * (d %>% pull(y) %>% last())), " ventiladores disponibles<br> por cada 100")),
     spark = hc,
     minititle = paste0("Ventiladores")
   )
