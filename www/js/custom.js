a22852f4ac7ecde92b4c81dd2bba755d895fd541
Joshua <jbkunst@gmail.com>
2020-06-08 23:56:05 -0400

    asdf

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   $("br").remove();
-  
+  /*  
   var t = setInterval(function(){
     var resizeEvent = new Event('resize');
     window.dispatchEvent(resizeEvent);  
     
   }, 1000);
+  
+  */
 
 });
